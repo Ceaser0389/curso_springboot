0989a8d5a1ff2f63c1735d96587b277426b24ff8
César Alves <ceaser.alves03@gmail.com>
2025-06-20 13:02:04 -0300

    Obtendo produto por id e persistindo produtos no banco

--- 01-produtosapi/src/main/java/io/cesa/produtosapi/controller/ProdutoController.java
+++ 01-produtosapi/src/main/java/io/cesa/produtosapi/controller/ProdutoController.java
@@ -1,21 +1,40 @@
 package io.cesa.produtosapi.controller;
 
 import io.cesa.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.cesa.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
 
-@RestController  ///  marca para ser um controlador
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController  // marca para ser um controlador
 @RequestMapping("produtos")  // mapeia url
 public class ProdutoController {
+
+  private ProdutoRepository produtoRepository;
+
+  public ProdutoController(ProdutoRepository produtoRepository) {
+    this.produtoRepository = produtoRepository;
+  }
 
   @PostMapping
   public Produto salvar ( @RequestBody Produto produto) {
     System.out.printf("Produto recebido" + produto);
+
+    var id = UUID.randomUUID().toString();
+    produto.setId(id);
+
+    produtoRepository.save(produto);
     return produto;
   }
 
+  @GetMapping("{id}")
+  public Produto obterPorId(@PathVariable("id")  String id){
+//    Optional<Produto> produto = produtoRepository.findById(id);
+//    return  produto.isPresent() ? produto.get() : null;
+    return  produtoRepository.findById(id).orElse(null);
+  }
 
 
 
--- 01-produtosapi/src/main/java/io/cesa/produtosapi/model/Produto.java
+++ 01-produtosapi/src/main/java/io/cesa/produtosapi/model/Produto.java
@@ -1,11 +1,26 @@
 package io.cesa.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 // POJO -> Plan old Java Object
+@Entity
+@Table  // não é obri se o nome da tabela é o mesmo da entidade
 public class Produto {
 
+   @Id
+   @Column(name = "id")
    private String id;
+
+   @Column(name = "nome")
    private String nome;
+
+   @Column(name = "descricao")
    private String descricao;
+
+   @Column(name = "preco")
    private Double preco;
 
    public String getId() {
